8c4f22e1e418d3c6dd32fbd2f20900e4ad502769
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-22 08:07:36 +0900

    ダッシュボードページに新規記事作成ボタンと投稿一覧を表示するテーブルを追加しました。テーブルには投稿のタイトル、表示状態、更新日時が含まれ、各投稿に対する操作用のドロップダウンメニューを設置予定です。

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import { getOwnPosts } from "@/lib/ownPost";
+import { tr } from "zod/v4/locales";
 
 export default async function DashboardPage() {
   const session = await auth();
@@ -10,7 +11,29 @@ export default async function DashboardPage() {
   const posts = await getOwnPosts(userId);
   return (
     <div>
-      <div>dashboard</div>
+      <div>
+        <button className="bg-amber-200 p-4">新規記事作成</button>
+      </div>
+      <table className="table-auto w-full border-collapse border">
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>表示/表示</th>
+            <th>更新日時</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.title}</td>
+              <td>{post.published ? "表示" : "非表示"}</td>
+              <td>{new Date(post.updatedAt).toLocaleString()}</td>
+              <td>{/* ここにドロップダウンメニューがはいります */}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
